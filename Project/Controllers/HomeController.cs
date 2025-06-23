e2ad79b1c763a86113aa27c712a5154fff7068a5
joey <joey4755@gmail.com>
2025-06-23 13:58:43 +0800

    修改HomeController

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -13,35 +13,36 @@ namespace Project.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DbuniPayContext _db;
+
+        public HomeController(ILogger<HomeController> logger,DbuniPayContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            DbuniPayContext db = new DbuniPayContext();
-
             var today = DateTime.Today;
             var currentMonth = DateTime.Today.Month;
             var currentYear = DateTime.Today.Year;
             var lastDayOfMonth = DateTime.DaysInMonth(currentYear, currentMonth);
 
-            List<Torder> order = db.Torders.ToList();
-            List<TorderDetail> orderDetail = db.TorderDetails.ToList();
-            List<Tproduct> product_none = db.Tproducts.Where(t => t.Pinventory == 0).ToList();
-            List<Torder> order_none = db.Torders.Where(t => t.Ostatus == "待出貨" && t.Odate <= today.AddDays(-3)).ToList();
+            List<Torder> order =await _db.Torders.ToListAsync();
+            List<TorderDetail> orderDetail =await _db.TorderDetails.ToListAsync();
+            List<Tproduct> product_none =await _db.Tproducts.Where(t => t.Pinventory == 0).ToListAsync();
+            List<Torder> order_none =await _db.Torders.Where(t => t.Ostatus == "待出貨" && t.Odate <= today.AddDays(-3)).ToListAsync();
             List<int> month_Days = new List<int>();
             List<int?> members_count_Month = new List<int?>();
             List<int?> members_count_Year = new List<int?>();
             List<int> product_inventory = new List<int>();
             List<string> product_name = new List<string>();
-            List<int> type_sales_count = (from tod in db.TorderDetails
-                                          join tp in db.Tproducts on tod.Pid equals tp.Pid
+            List<int> type_sales_count =await (from tod in _db.TorderDetails
+                                          join tp in _db.Tproducts on tod.Pid equals tp.Pid
                                           group tod by tp.Pcategory into grouped
                                           select grouped.Sum(t => t.Pcount)
                                             )
-                                            .ToList();
+                                            .ToListAsync();
 
             int sumday = 0;
             int summonth = 0;
@@ -50,7 +51,7 @@ namespace Project.Controllers
             {
                 if (day <= today.Day)
                 {
-                    sumday += db.Tmembers.Count(t => t.McreatedDate.Month == currentMonth && t.McreatedDate.Year == currentYear && t.McreatedDate.Day == day);
+                    sumday += _db.Tmembers.Count(t => t.McreatedDate.Month == currentMonth && t.McreatedDate.Year == currentYear && t.McreatedDate.Day == day);
                     members_count_Month.Add(sumday);
                 }
                 else
@@ -66,7 +67,7 @@ namespace Project.Controllers
             {
                 if (month <= currentMonth)
                 {
-                    summonth += db.Tmembers.Count(t => t.McreatedDate.Month == month && t.McreatedDate.Year == currentYear);
+                    summonth += _db.Tmembers.Count(t => t.McreatedDate.Month == month && t.McreatedDate.Year == currentYear);
                     members_count_Year.Add(summonth);
                 }
                 else
@@ -76,11 +77,11 @@ namespace Project.Controllers
 
             }
 
-            product_inventory.AddRange(db.Tproducts.Where(t => t.Pinventory <= 30).Select(t => t.Pinventory).ToList());
-            product_name.AddRange(db.Tproducts.Where(t => t.Pinventory <= 30).Select(t => t.Pname).ToList());
+            product_inventory.AddRange(_db.Tproducts.Where(t => t.Pinventory <= 30).Select(t => t.Pinventory).ToList());
+            product_name.AddRange(_db.Tproducts.Where(t => t.Pinventory <= 30).Select(t => t.Pname).ToList());
 
-            ViewBag.SalesCount_TOP5_Today = (from tod in db.TorderDetails
-                                             join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.SalesCount_TOP5_Today =await (from tod in _db.TorderDetails
+                                             join td in _db.Torders on tod.Oid equals td.Oid
                                              where td.Odate.Date == today && tod.Pid != 0
                                              group tod by tod.Pid into grouped
                                              select new
@@ -94,10 +95,10 @@ namespace Project.Controllers
                                              })
                                              .OrderByDescending(x => x.count)
                                              .Take(5)
-                                             .ToList();
+                                             .ToListAsync();
 
-            ViewBag.SalesCount_TOP5_Month = (from tod in db.TorderDetails
-                                             join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.SalesCount_TOP5_Month = await (from tod in _db.TorderDetails
+                                             join td in _db.Torders on tod.Oid equals td.Oid
                                              where td.Odate.Month == currentMonth && td.Odate.Year == currentYear && tod.Pid != 0
 											 group tod by tod.Pid into grouped
                                              select new
@@ -111,10 +112,10 @@ namespace Project.Controllers
                                              })
                                              .OrderByDescending(x => x.count)
                                              .Take(5)
-                                             .ToList();
+                                             .ToListAsync();
 
-            ViewBag.SalesCount_TOP5_Year = (from tod in db.TorderDetails
-                                            join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.SalesCount_TOP5_Year =await (from tod in _db.TorderDetails
+                                            join td in _db.Torders on tod.Oid equals td.Oid
                                             where td.Odate >= new DateTime(2025, 1, 1) && td.Odate <= new DateTime(2025, 12, 31) && tod.Pid != 0
 											group tod by tod.Pid into grouped
                                             select new
@@ -128,11 +129,11 @@ namespace Project.Controllers
                                             })
                                             .OrderByDescending(x => x.count)
                                             .Take(5)
-                                            .ToList();
+                                            .ToListAsync();
 
-            ViewBag.typesales_today = (from tod in db.TorderDetails
-                                       join tp in db.Tproducts on tod.Pid equals tp.Pid
-                                       join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.typesales_today =await (from tod in _db.TorderDetails
+                                       join tp in _db.Tproducts on tod.Pid equals tp.Pid
+                                       join td in _db.Torders on tod.Oid equals td.Oid
                                        where td.Odate.Date == today
                                        group new { tod, tp } by tp.Pcategory into grouped
                                        select new
@@ -141,11 +142,11 @@ namespace Project.Controllers
                                            name = grouped.Key // 分組鍵即為 Pcategory
                                        }
                                         )
-                                        .ToList();
+                                        .ToListAsync();
 
-            ViewBag.typesales_month = (from tod in db.TorderDetails
-                                       join tp in db.Tproducts on tod.Pid equals tp.Pid
-                                       join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.typesales_month =await (from tod in _db.TorderDetails
+                                       join tp in _db.Tproducts on tod.Pid equals tp.Pid
+                                       join td in _db.Torders on tod.Oid equals td.Oid
                                        where td.Odate.Month == currentMonth && td.Odate.Year == currentYear
                                        group new { tod, tp } by tp.Pcategory into grouped
                                        select new
@@ -154,11 +155,11 @@ namespace Project.Controllers
                                            name = grouped.Key // 分組鍵即為 Pcategory
                                        }
                                         )
-                                        .ToList();
+                                        .ToListAsync();
 
-            ViewBag.typesales_year = (from tod in db.TorderDetails
-                                      join tp in db.Tproducts on tod.Pid equals tp.Pid
-                                      join td in db.Torders on tod.Oid equals td.Oid
+            ViewBag.typesales_year =await (from tod in _db.TorderDetails
+                                      join tp in _db.Tproducts on tod.Pid equals tp.Pid
+                                      join td in _db.Torders on tod.Oid equals td.Oid
                                       where td.Odate.Year == currentYear
                                       group new { tod, tp } by tp.Pcategory into grouped
                                       select new
@@ -167,29 +168,29 @@ namespace Project.Controllers
                                           name = grouped.Key // 分組鍵即為 Pcategory
                                       }
                                         )
-                                        .ToList();
+                                        .ToListAsync();
 
-            int totalprice_today = db.Torders.Where(t => t.Odate.Date == today).Sum(t => t.OtotalPrice);
-            int totalprice_month = db.Torders.Where(t => t.Odate.Month == currentMonth && t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
-            int totalprice_year = db.Torders.Where(t => t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
-            int totalprice_yesterday = db.Torders.Where(t => t.Odate.Date == today.AddDays(-1)).Sum(t => t.OtotalPrice);
-            int totalprice_lastmonth = db.Torders.Where(t => t.Odate.Month == currentMonth - 1 && t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
-            int totalprice_lastyear = db.Torders.Where(t => t.Odate.Year == currentYear - 1).Sum(t => t.OtotalPrice);
+            int totalprice_today = _db.Torders.Where(t => t.Odate.Date == today).Sum(t => t.OtotalPrice);
+            int totalprice_month = _db.Torders.Where(t => t.Odate.Month == currentMonth && t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
+            int totalprice_year = _db.Torders.Where(t => t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
+            int totalprice_yesterday = _db.Torders.Where(t => t.Odate.Date == today.AddDays(-1)).Sum(t => t.OtotalPrice);
+            int totalprice_lastmonth = _db.Torders.Where(t => t.Odate.Month == currentMonth - 1 && t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
+            int totalprice_lastyear = _db.Torders.Where(t => t.Odate.Year == currentYear - 1).Sum(t => t.OtotalPrice);
 
-            int totalsales_today = db.Torders.Where(t => t.Odate.Date == today).Count();
-            int totalsales_month = db.Torders.Where(t => t.Odate.Month == currentMonth && t.Odate.Year == currentYear).Count();
-            int totalsales_year = db.Torders.Where(t => t.Odate.Year == currentYear).Count();
-            int totalsales_yesterday = db.Torders.Where(t => t.Odate.Date == today.AddDays(-1)).Count();
-            int totalsales_lastmonth = db.Torders.Where(t => t.Odate.Month == currentMonth - 1 && t.Odate.Year == currentYear).Count();
-            int totalsales_lastyear = db.Torders.Where(t => t.Odate.Year == currentYear - 1).Count();
+            int totalsales_today = _db.Torders.Where(t => t.Odate.Date == today).Count();
+            int totalsales_month = _db.Torders.Where(t => t.Odate.Month == currentMonth && t.Odate.Year == currentYear).Count();
+            int totalsales_year = _db.Torders.Where(t => t.Odate.Year == currentYear).Count();
+            int totalsales_yesterday = _db.Torders.Where(t => t.Odate.Date == today.AddDays(-1)).Count();
+            int totalsales_lastmonth = _db.Torders.Where(t => t.Odate.Month == currentMonth - 1 && t.Odate.Year == currentYear).Count();
+            int totalsales_lastyear = _db.Torders.Where(t => t.Odate.Year == currentYear - 1).Count();
 
 
-            int Advice_today = db.Tadvices.Where(t => t.DateTime.Date == today).Count();
-            int Advice_month = db.Tadvices.Where(t => t.DateTime.Month == currentMonth && t.DateTime.Year == currentYear).Count();
-            int Advice_year = db.Tadvices.Where(t => t.DateTime.Year == currentYear).Count();
-            int CustService_yesterday = db.Tchats.Where(t => t.ChatCreateTime.Date == today.AddDays(-1)).Count();
-            int CustSercive_lastmonth = db.Tchats.Where(t => t.ChatCreateTime.Month == currentMonth - 1 && t.ChatCreateTime.Year == currentYear).Count();
-            int CustSercive_lastyear = db.Tchats.Where(t => t.ChatCreateTime.Year == currentYear - 1).Count();
+            int Advice_today = _db.Tadvices.Where(t => t.DateTime.Date == today).Count();
+            int Advice_month = _db.Tadvices.Where(t => t.DateTime.Month == currentMonth && t.DateTime.Year == currentYear).Count();
+            int Advice_year = _db.Tadvices.Where(t => t.DateTime.Year == currentYear).Count();
+            int CustService_yesterday = _db.Tchats.Where(t => t.ChatCreateTime.Date == today.AddDays(-1)).Count();
+            int CustSercive_lastmonth = _db.Tchats.Where(t => t.ChatCreateTime.Month == currentMonth - 1 && t.ChatCreateTime.Year == currentYear).Count();
+            int CustSercive_lastyear = _db.Tchats.Where(t => t.ChatCreateTime.Year == currentYear - 1).Count();
 
 
             var viewModel = new IndexViewModel
@@ -232,8 +233,8 @@ namespace Project.Controllers
 		[HttpPost]
 		public IActionResult Login(MemberViewModel m)//存入快取
 		{
-			DbuniPayContext db = new DbuniPayContext();
-			Tmember T = db.Tmembers.FirstOrDefault(c => c.Maccount == m.Account && c.Mpassword == m.Password);
+			
+			Tmember T = _db.Tmembers.FirstOrDefault(c => c.Maccount == m.Account && c.Mpassword == m.Password);
 			string Error = "false";
 			if (T != null && T.Mpassword == m.Password)
 			{
