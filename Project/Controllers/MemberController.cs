66f9830313b1f6baa0ae7f40167912ed1a8f1eb0
joey <joey4755@gmail.com>
2026-04-20 23:35:44 +0800

    test1

--- Project/Controllers/MemberController.cs
+++ Project/Controllers/MemberController.cs
@@ -83,6 +83,8 @@ namespace Project.Controllers
                 try
                 {
 					_db.SaveChanges(); // 保存變更
+                    var json = HttpContext.Session.GetString(CDictionary.SK_LOGEDIN_USER);
+                    var currentUser = JsonSerializer.Deserialize<Tmember>(json);
                     var blacklistedMembers = _db.Tmembers.Where(m => m.MisHided == true).ToList();
                     List<CMemberWrap> list = blacklistedMembers.Select(t => new CMemberWrap { member = t }).ToList();
                 }
